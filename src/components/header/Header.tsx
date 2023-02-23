048d72a8772986246c26bfebfe2b2161cf206ea9
Tomas Floxo <tomasfloxolodeiro@gmail.com>
2023-02-23 14:42:12 -0300

    Header container

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,17 +1,17 @@
-import { AppBar } from "@mui/material";
+import { AppBar, Grid } from "@mui/material";
 import {ReactComponent as PBLogo} from "./pblogo-whiteborder.svg"
 import { useTranslation } from 'react-i18next';
-import './Header.css';
 
 export const Header = () => {
     const { t } = useTranslation("header");
     
     
-    return <AppBar position="fixed" className="app-bar" >
-        {t('title')}
-        <PBLogo height="50px" />
-    </AppBar>
-
+    return <AppBar position="fixed">
+            <Grid container justifyContent="space-between">
+                <PBLogo height="50px"/>
+                <div>{t('title')}</div>
+            </Grid>
+        </AppBar>
 }
 
 
