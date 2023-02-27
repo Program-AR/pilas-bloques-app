eda18bde0ad04da12b465069be18b7a6d8c8d7d8
Diana López Alvas <dlopezalvas@gmail.com>
2023-02-27 15:23:05 -0300

    Using font Nunito

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
-import { CssBaseline } from '@mui/material';
+import { CssBaseline, ThemeProvider } from '@mui/material';
 import React from 'react';
 import './App.css';
 import { ChallengeView } from './components/ChallengeView';
 import { Header } from './components/Header';
+import theme from './theme';
 
 function App() {
   return (
     <React.Fragment>
-      <CssBaseline />
-      <Header/>
-      <ChallengeView/>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Header/>
+        <ChallengeView/>
+      </ThemeProvider>
     </React.Fragment>
   );
 }
---
+++ src/theme.js
@@ -0,0 +1,10 @@
+import { createTheme } from "@mui/material/styles"
+
+const theme = createTheme({
+    typography: {
+        fontFamily: 'Nunito'
+    },
+
+})
+
+export default theme
