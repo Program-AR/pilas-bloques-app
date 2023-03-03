8111efd68a52f0a6ff553331fcb3912512ba3029
Diana López Alvas <dlopezalvas@gmail.com>
2023-03-03 12:06:56 -0300

    header pb style

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,15 +2,15 @@ import { AppBar, Grid } from "@mui/material";
 import {ReactComponent as PBLogo} from "./pblogo-whiteborder.svg"
 import { useTranslation } from 'react-i18next';
 import { ChangeLanguageButton } from "./ChangeLanguageButton";
+import styles from './header.module.css';
 
 export const Header = () => {
     const { t } = useTranslation("header");
     
-    
-    return <AppBar position="fixed">
-            <Grid container justifyContent="space-between">
-                <PBLogo height="50px"/>
-                <p align-content="center">{t('tool')}</p>
+    return <AppBar position="fixed" elevation={0}>
+            <Grid container className={styles.header}>
+                <PBLogo className={styles.logo}/>
+                <p className={styles.headerTitle}>{t('tool')}</p>
                 <ChangeLanguageButton/>
             </Grid>
         </AppBar>
