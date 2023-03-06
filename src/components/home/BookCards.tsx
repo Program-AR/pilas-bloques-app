dd3344ea67ddea87debb4df67e70a205a4646b9a
Tomas Floxo <tomasfloxolodeiro@gmail.com>
2023-03-06 11:17:36 -0300

    Translating cards

--- src/components/home/BookCards.tsx
+++ src/components/home/BookCards.tsx
@@ -3,25 +3,30 @@ import TotoInitial from "../../assets/toto-initial-level.svg"
 import TotoIntermediate from "../../assets/toto-intermediate-level.svg"
 import TotoAdvanced from "../../assets/toto-advanced-level.svg"
 import { Link } from "react-router-dom";
+import { useTranslation } from "react-i18next";
 
 type BookCardProps = {
     url: string,
-    name: string,
+    nameKey: string,
     image: string,
     color: string 
 }
 
-const BookCard = (props: BookCardProps) => 
+const BookCard = (props: BookCardProps) => {
+    const { t } = useTranslation("bookCards");
+
+    return (
     <Link to={props.url}>
-        <Card style={{minHeight:"10rem", minWidth:"8rem", backgroundColor: props.color}}>
+        <Card style={{ width:"9rem", backgroundColor: props.color}}>
             <img src={props.image}></img>
-            <Typography variant="h5" align="center">{props.name}</Typography>
+            <Typography variant="h5" align="center">{t(props.nameKey)}</Typography>
         </Card>
     </Link>
-
-const InitialBookCard = () => <BookCard url="libros/1" name="Inicial" color="#FCE43E" image={TotoInitial}/>
-const IntermediateBookCard = () => <BookCard url="libros/2" name="Intermedio" color="#53BF24" image={TotoIntermediate}/>
-const AdvancedBookCard = () => <BookCard url="libros/100" name="Avanzado" color="#32CFC1" image={TotoAdvanced}/>
+    )
+}
+const InitialBookCard = () => <BookCard url="libros/1" nameKey="initial" color="#FCE43E" image={TotoInitial}/>
+const IntermediateBookCard = () => <BookCard url="libros/2" nameKey="intermediate" color="#53BF24" image={TotoIntermediate}/>
+const AdvancedBookCard = () => <BookCard url="libros/100" nameKey="advanced" color="#32CFC1" image={TotoAdvanced}/>
 
 export const BookCards = () =>
     <Stack direction="row" spacing={10}>
