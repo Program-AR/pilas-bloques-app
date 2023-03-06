95953e3b449b262f9af1f4003045a7eb7aea3f3c
Tomas Floxo <tomasfloxolodeiro@gmail.com>
2023-03-06 11:45:13 -0300

    more more more translations

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,16 +1,18 @@
 import { Stack, Grid, Typography, Link } from "@mui/material";
+import { useTranslation } from "react-i18next";
 import {ReactComponent as FooterLogos} from "../../assets/footer-logos.svg"
 
 const Version = () => 
     <div>Versión 1.2.3 - pepita</div>
 
 const Links = () =>{
+    const {t} = useTranslation("footer")
     const termsAndConditionsLink = "https://docs.google.com/document/u/1/d/e/2PACX-1vTNX9zl8txZmuINNz2qODrodoQhvr0o2-r3T_6yFp6quEpidmPz6ORx1HSjo2KNUg6MnyHPN-Ti44z1/pub"
     
     return <>
-        <Link href="acercade">Acerca de Pilas Bloques</Link> |
-        <Link href="docentes"> Ir al sitio para docentes</Link> |
-        <Link href={termsAndConditionsLink}> Términos y condiciones</Link>
+        <Link href="acercade">{t("aboutPilasBloques")}</Link> |
+        <Link href="docentes"> {t("toTeachersSite")}</Link> |
+        <Link href={termsAndConditionsLink}> {t("terms")}</Link>
     </>
 }
 
