1faded5e6c54f28dce6cee17c8715360c8da6c8f
Tomas Floxo <tomasfloxolodeiro@gmail.com>
2023-03-28 16:09:02 -0300

    More error

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ function App() {
     {
       path: "",
       element: <Home/>,
+      errorElement: <PBError />
     },
     {
       path: "/libros/:id",
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,8 +1,9 @@
-import { AppBar, Grid, Link } from "@mui/material";
+import { AppBar, Grid } from "@mui/material";
 import {ReactComponent as PBLogo} from "../../assets/pblogo-whiteborder.svg"
 import { useTranslation } from 'react-i18next';
 import { ChangeLanguageButton } from "./ChangeLanguageButton";
 import styles from './header.module.css';
+import { Link } from "react-router-dom";
 
 type HeaderProps = {
     CenterComponent: React.ReactNode
@@ -13,7 +14,7 @@ export const Header = (props: HeaderProps) => {
     
     return <AppBar position="fixed" elevation={0}>
             <Grid container className={styles.header}>
-                <Link href="#"><PBLogo className={styles.logo}/></Link>
+                <Link to="/"><PBLogo className={styles.logo}/></Link>
                 {props.CenterComponent}
                 <ChangeLanguageButton/>
             </Grid>
