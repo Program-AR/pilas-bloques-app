2735c3238aaf4dab9c89ab25a92701fd0c4d3960
Alfredo Sanzo <alfredo.sanzo@gmail.com>
2023-03-28 23:33:33 -0300

    Simplify packaging script. Deprecating Linux ia32

--- scripts/package.sh
+++ scripts/package.sh
@@ -1,128 +1,99 @@
 #!/bin/bash
 
+# The folder with all the publishable webapp
+DIST=build
+[[ -d $DIST ]] || { echo "ERROR: The folder $DIST doesn't exist. You have to build Pilas Bloques before packaging." ; exit 1; }
+
 #The project name, used by electron-packager in order to create files and folders for the app binaries.
 NAME=$(sh scripts/projectName.sh)
-
-DIST_FOLDER_NAME=build
-
-# The folder with all the publishable webapp
-DIST=./$DIST_FOLDER_NAME
-
 # The project version.
-VERSION="$(sh scripts/projectVersion.sh)$(node scripts/fixingAbsolutePath.js $DIST)"
+VERSION="$(sh scripts/projectVersion.sh)"
 
-# The electron executable path.
-ELECTRON=./node_modules/dist/electron
-
-# The electron packager path.
-PACKAGER=./node_modules/.bin/electron-packager
-
-# The electron debian packager path.
-DEBIAN_PACKAGER=./node_modules/.bin/electron-installer-debian
-
-[[ -d $DIST ]] || { echo "ERROR: The folder $DIST doesn't exist. You have to build Pilas Bloques before packaging." ; exit 1; }
+eco(){
+    echo ""
+    echo "==> |"$(date "+%F|%T%z")"| "$1
+}
 
 help() {
     echo ""
-	echo "Commands available for pilas-bloques - versión $VERSION"
+	echo "Commands available for Pilas Bloques - version $VERSION"
 	echo ""
 	echo "  for packaging :"
 	echo ""
-	echo "    -osx              Make a electron osx electron package."
-	echo "    -linux_x64_deb    Make a electron linux x64 deb electron package."
-	echo "    -linux_x64_zip    Make a electron linux x64 zip electron package."
-	echo "    -linux_ia32_zip   Make a electron linux x32 zip electron package."
-	echo "    -win32            Make a electron Win32 electron package."
-	echo "    -linux            Make all electron linux electron packages."
-    echo "    -html             Make a html package"
+	echo "    -osx         Packs w/ Electron and makes an OSX dmg installer."
+	echo "    -linux_deb   Packs w/ Electron and makes a Debian deb x64 installer."
+	echo "    -linux_zip   Packs w/ Electron and makes a zipfile linux x64 executable."
+	echo "    -linux       linux_deb + linux_zip"
+	echo "    -win32       Packs w/ Electron and makes a Win32 installer."
+    echo "    -html        Makes zipfile with html to serve."
 	echo ""
 	echo "  NOTE: every version generates a binary automatically in CI."
 	echo ""
-	echo ""
 }
 
-prebuild() {
-    PLATFORM=$1
-    ARCH=$2
-    
-    echo "Generating package for $PLATFORM $ARCH..."
+prepack() {
     mkdir -p ./binaries
     cp package.json $DIST/package.json
     cp packaging/electron.js $DIST
     cp -R locales $DIST
+    node scripts/fixingAbsolutePath.js $DIST
 }
 
-package() {
+pack() {
     PLATFORM=$1
     ARCH=$2
     ICON_EXTENSION=$3
 
-    $PACKAGER $DIST $NAME --app-version=$VERSION --platform=$PLATFORM --arch=$ARCH --ignore=node_modules --out=binaries --overwrite --icon=packaging/icono.$ICON_EXTENSION
+    prepack
+    ./node_modules/.bin/electron-packager ./$DIST $NAME --app-version=$VERSION --platform=$PLATFORM --arch=$ARCH --ignore=node_modules --out=binaries --overwrite --icon=packaging/icono.$ICON_EXTENSION
 }
 
-package_linux_x64_deb() {
-    prebuild "linux" "x64"
-    echo "Generating linux x64 package for debian..."
+linux_deb() {
+    eco "Generating linux x64 debian installer..."
     rm -f "./binaries/$NAME_$VERSION_amd64.deb"
-    package "linux" "x64" "icns"
-    $DEBIAN_PACKAGER --arch amd64 --config=packaging/linux-package.json
+    pack "linux" "x64" "icns"
+    ./node_modules/.bin/electron-installer-debian --arch amd64 --config=packaging/linux-package.json
 }
 
-package_linux_ia32_zip() {
-    prebuild "linux" "ia32"
-    echo "Generating linux ia32 zip..."
-    rm -f ./binaries/$NAME-$VERSION-ia32.zip
-    package "linux" "ia32" "icns"
-    cd binaries; zip -r $NAME-$VERSION-linux-ia32.zip $NAME-linux-ia32/; cd ..
-}
-
-package_linux_x64_zip() {
-    prebuild "linux" "x64"
-    echo "Generating linux x64 zip..."
+linux_zip() {
+    eco "Generating linux x64 zip..."
     rm -f ./binaries/$NAME-$VERSION-x64.zip
-    package "linux" "x64" "icns"
+    pack "linux" "x64" "icns"
     cd binaries; zip -r $NAME-$VERSION-linux-x64.zip $NAME-linux-x64/; cd ..
 }
 
-package_linux() {
-    package_linux_x64_deb
-    package_linux_ia32_zip
-    package_linux_x64_zip
+linux() {
+    linux_zip
+    linux_deb
 }
 
-package_osx() {
-    prebuild "darwin" "all"
-    echo "Generating package for osx..."
-    package "darwin" "all" "icns"
-    # TODO
-    # se quita esta linea ya que hdiutil es copyright protected by Apple y no existe mas
-    # se deberia conseguir otro empaquetador para generar el dmg
+osx() {
+    eco "Generating installer for osx..."
+    pack "darwin" "all" "icns"
     hdiutil create binaries/$NAME-$VERSION.dmg -srcfolder ./binaries/$NAME-darwin-x64/$NAME.app -size 1g
 }
 
-package_win32() {
-    prebuild "win" "ia32"
-    echo "Generating installer for windows package..."
-    package "win32" "ia32" "ico"
+win32() {
+    eco "Generating installer for windows..."
+    pack "win32" "ia32" "ico"
 	cp packaging/instalador.nsi binaries/$NAME-win32-ia32/
 	cd binaries/$NAME-win32-ia32/; makensis instalador.nsi; cd ../..
 	mv binaries/$NAME-win32-ia32/$NAME.exe binaries/$NAME-$VERSION.exe
 }
 
-package_html() {
-    echo "Generating package for html..."
+html() {
+    eco "Generating html zip from build folder..."
     mkdir -p ./binaries
-    tar czf ./binaries/$NAME-$VERSION-html.tar.gz $DIST_FOLDER_NAME # ./ should be ommited, otherwise it will be inside tar.gz archive.
+    tar czf ./binaries/$NAME-$VERSION-html.tar.gz $DIST
 }
 
 case "$1" in
-    (-help)             help;;
-    (-linux_x64_deb)    package_linux_x64_deb;;
-    (-linux_ia32_zip)   package_linux_ia32_zip;;
-    (-linux_x64_zip)    package_linux_x64_zip;;
-    (-linux)            package_linux;;
-    (-osx)              package_osx;;
-    (-win32)            package_win32;;
-    (-html)             package_html;;
-    (*)                 echo Try 'package -help' for more information.;;
+    (-help)        help;;
+    (-linux_deb)   linux_deb;;
+    (-linux_zip)   linux_zip;;
+    (-linux)       linux;;
+    (-osx)         osx;;
+    (-win32)       win32;;
+    (-html)        html;;
+    (*)            help;;
 esac 
