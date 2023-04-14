22a4ab8921a3db5b0aa22036158bcd2d8a1b9c01
Tomas Floxo <37090248+tfloxolodeiro@users.noreply.github.com>
2023-04-14 11:19:16 -0300

    Disabling i18n debug

--- src/i18n.ts
+++ src/i18n.ts
@@ -15,7 +15,7 @@ i18n
   // init i18next
   // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
-    debug: true,
+    debug: false,
     lowerCaseLng: true,
     fallbackLng: spanish.languageCode,
     lng: selectedLanguage() || spanish.languageCode,
