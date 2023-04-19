c3bbf177d59cd6b74078953a6134a48f8dbc2b22
Tomas Floxo <tomasfloxolodeiro@gmail.com>
2023-04-19 12:09:16 -0300

    Narrowing serialized challenge

--- src/components/home/ImportChallengeCard.tsx
+++ src/components/home/ImportChallengeCard.tsx
@@ -3,51 +3,10 @@ import ImportImage from "../../assets/import.png"
 import Button from '@mui/material/Button';
 import { Ember } from "../../emberCommunication";
 import { useNavigate } from "react-router-dom";
-import simpleTypeGuard, { SimpleArray, SimpleStringOptional, SimpleBoolean, SimpleNumber, SimpleString, SimpleBooleanOptional, SimpleObjectOptional, SimpleExactMatch } from 'simple-type-guard';
 import { useState } from "react";
 import { Modal, Paper, Typography } from "@mui/material";
 import { useTranslation } from "react-i18next";
-
-
-type SceneType = "lita" | "duba" | "toto" | "coty" | "niandu" | "pinguine" | "yaguarete" | "carpincho" | "custom"
-const sceneTypes: SceneType[] =  ["lita", "duba", "toto", "coty", "niandu", "pinguine", "yaguarete", "carpincho", "custom"] //Used for file validity checking
-
-type Cell = "P" | "-" | "A" | "" // should consider different objects / empty cell / no cell at all
-const cells: Cell[] = ["P", "-", "A", ""] //Used for file validity checking
-
-type SceneMap = Cell[]
-
-export type Scene = {
-    type: SceneType
-    maps: SceneMap[]
-}
-
-type Assesments =  {
-    itWorks?: boolean, // old "debeFelicitar", default true
-    decomposition?: DecompositionAssessment,
-    simpleRepetition?: boolean,
-    conditionalRepetition?: boolean,
-    conditionalAlternative?: boolean,
-}
-
-type DecompositionAssessment = { maxProgramLength: number }
-
-export type SerializedChallenge = {
-    fileVersion: number,
-    title: string,
-    statement: {
-        description: string,
-        clue?: string
-    },
-    scene: Scene,
-    toolbox: {
-        blocks: string[], // for now, block ids, future: could be objects.
-        categorized?: boolean // default true
-    },
-    stepByStep?: boolean, // default false
-    predefinedSolution?: string,
-    assesments?: Assesments
-}
+import { SerializedChallenge, isValidChallenge } from "../serializedChallenge";
 
 export const ImportChallengeCard = () => {
     const navigate = useNavigate();
@@ -62,30 +21,6 @@ export const ImportChallengeCard = () => {
         setModalOpen(true)
     }
 
-    const isValidChallenge = (json: unknown): boolean => 
-        simpleTypeGuard<SerializedChallenge>(json, {
-            fileVersion: SimpleNumber,
-            title: SimpleString,
-            statement: {description: SimpleString, clue: SimpleStringOptional},
-            toolbox: {blocks: new SimpleArray(SimpleString), categorized: SimpleBooleanOptional},
-            stepByStep: SimpleBooleanOptional,
-            predefinedSolution: SimpleStringOptional,
-            scene: { 
-                type: SimpleString, //TODO: Poner los exact match
-                maps: new SimpleArray<Cell[]>(new SimpleArray<Cell>(SimpleString)) //TODO: Poner los exact match
-            },
-            assesments: new SimpleObjectOptional<Assesments>({
-                itWorks: SimpleBooleanOptional,
-                simpleRepetition: SimpleBooleanOptional,
-                conditionalRepetition: SimpleBooleanOptional,
-                conditionalAlternative: SimpleBooleanOptional,
-                decomposition: new SimpleObjectOptional<DecompositionAssessment>({
-                    maxProgramLength: SimpleNumber
-                })
-
-            })
-        })
-
     const readFile = async (event: any) => {
         const file: File = event.target.files[0]
         const content: string = await file.text()
@@ -94,7 +29,7 @@ export const ImportChallengeCard = () => {
         event.target.value = null // Without this Chrome seems to cache the file and prevents reruns of this function. 
 
         if (isValidChallenge(challengeJson)) {
-            goToChallenge(challengeJson as SerializedChallenge)
+            goToChallenge(challengeJson)
         }
         else {
             showErrorModal()
---
+++ src/components/serializedChallenge.ts
@@ -0,0 +1,98 @@
+import simpleTypeGuard, { SimpleArray, SimpleStringOptional, SimpleNumber, SimpleString, SimpleBooleanOptional, SimpleObjectOptional, SimpleExactMatch } from 'simple-type-guard';
+
+
+export type SerializedChallenge = {
+    fileVersion: number,
+    title: string,
+    statement: {
+        description: string,
+        clue?: string
+    },
+    scene: Scene,
+    toolbox: {
+        blocks: string[], // for now, block ids, future: could be objects.
+        categorized?: boolean // default true
+    },
+    stepByStep?: boolean, // default false
+    predefinedSolution?: string,
+    assesments?: Assesments
+}
+
+
+const sceneTypes =  ["lita", "duba", "toto", "coty", "niandu", "pinguine", "yaguarete", "carpincho", "custom"] as const //Used for file validity checking
+type SceneType = typeof sceneTypes[number]
+
+const cells = ["P", "-", "A", ""] as const //Used for file validity checking. Should consider different objects / empty cell / no cell at all
+type Cell = typeof cells[number]
+
+type SceneMap = Cell[]
+
+type Scene = {
+    type: SceneType
+    maps: SceneMap[]
+}
+
+type Assesments =  {
+    itWorks?: boolean, // old "debeFelicitar", default true
+    decomposition?: DecompositionAssessment,
+    simpleRepetition?: boolean,
+    conditionalRepetition?: boolean,
+    conditionalAlternative?: boolean,
+}
+
+type DecompositionAssessment = { maxProgramLength: number }
+
+
+export const isValidChallenge = (json: unknown): json is SerializedChallenge => {
+    const structureIsValid: boolean = simpleTypeGuard<SerializedChallenge>(json, {
+        fileVersion: SimpleNumber,
+        title: SimpleString,
+        statement: {description: SimpleString, clue: SimpleStringOptional},
+        toolbox: {blocks: new SimpleArray(SimpleString), categorized: SimpleBooleanOptional},
+        stepByStep: SimpleBooleanOptional,
+        predefinedSolution: SimpleStringOptional,
+        scene: { //We cant check if the scene values are valid here, so we need to check them manually.
+            type: SimpleString,
+            maps: new SimpleArray<Cell[]>(new SimpleArray<Cell>(SimpleString)) 
+        },
+        assesments: new SimpleObjectOptional<Assesments>({
+            itWorks: SimpleBooleanOptional,
+            simpleRepetition: SimpleBooleanOptional,
+            conditionalRepetition: SimpleBooleanOptional,
+            conditionalAlternative: SimpleBooleanOptional,
+            decomposition: new SimpleObjectOptional<DecompositionAssessment>({
+                maxProgramLength: SimpleNumber
+            })
+
+        })
+    })
+
+    return structureIsValid && sceneIsValid((json as any).scene)
+}
+
+const sceneIsValid = (serializedScene: any): serializedScene is Scene => {
+    //TODO: Poner el simple typeguard aca
+
+    const type: string | SceneType = serializedScene.type
+    const maps: string[][] | SceneMap[] = serializedScene.maps
+
+    if (!isSceneType(type)) return false
+    if (!isSceneMaps(maps)) return false
+
+    const scene: Scene = {type, maps} //This line is needed to let typescript verify on compile time if we satisfy the type of Scene.
+
+    return !!scene
+}
+    
+
+const isSceneType = (type: any): type is SceneType => 
+    sceneTypes.includes(type)
+
+const isSceneMaps = (maps: any): maps is SceneMap[] => 
+    maps.all(isSceneMap)
+
+const isSceneMap = (map: any): map is SceneMap =>
+    map.all(isCell)
+
+const isCell = (cell: any): cell is Cell =>
+    cells.includes(cell)
