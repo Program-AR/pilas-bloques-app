e6275eabd511ea90f0b9dd45beddc65e25cf8f64
Tomas Floxo <37090248+tfloxolodeiro@users.noreply.github.com>
2023-05-04 12:25:51 -0300

    Update src/components/creator/CreatorEditor.tsx
    Co-authored-by: asanzo <alfredo.sanzo@gmail.com>

--- src/components/creator/CreatorEditor.tsx
+++ src/components/creator/CreatorEditor.tsx
@@ -1,7 +1,9 @@
 import { Header } from "../header/Header"
+import Placeholder from "../../assets/placeholder.png"
 
 export const CreatorEditor = () =>{
     return <>
     <Header/>
+     <img alt={"neco"} src={`.${Placeholder}`}/>
 </>
 }
