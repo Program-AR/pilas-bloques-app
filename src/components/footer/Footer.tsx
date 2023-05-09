69e9c7557e8f4251711af71b641b0461386f27cb
Tomas Floxo <tomasfloxolodeiro@gmail.com>
2023-05-09 11:29:01 -0300

    Version in footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,34 +1,37 @@
-import { Stack, Grid, Box, Typography } from "@mui/material";
+import { Stack, Grid, Typography } from "@mui/material";
 import { useTranslation } from "react-i18next";
-import styles from './footer.module.css';
-import { ReactComponent as FooterLogos } from "../../assets/footer-logos.svg"
+import {ReactComponent as FooterLogos} from "../../assets/footer-logos.svg"
 import { Link } from "react-router-dom";
-import theme from '../../theme';
+import GitInfo from 'react-git-info/macro';
 
-const Version = () =>
-  <Typography className={styles["version"]} 
-              sx={{ [theme.breakpoints.down('sm')]: {fontSize: '12px'}}}>
-    Versión 1.2.3 - pepita
-  </Typography>
-
-const Links = () => {
-  const { t } = useTranslation("footer")
-  const termsAndConditionsLink = "https://docs.google.com/document/u/1/d/e/2PACX-1vTNX9zl8txZmuINNz2qODrodoQhvr0o2-r3T_6yFp6quEpidmPz6ORx1HSjo2KNUg6MnyHPN-Ti44z1/pub"
-
-  return <Box className={styles['footer-links']}>
-    <Link to="acercade">{t("aboutPilasBloques")}</Link> |
-    <Link to="https://pilasbloques.program.ar/docentes" target="_blank"> {t("toTeachersSite")}</Link> |
-    <Link to={termsAndConditionsLink} target="_blank"> {t("terms")}</Link>
-  </Box>
+const Version = () => {
+    const gitInfo = GitInfo()
+    const repoUrl = `https://github.com/Program-AR/pilas-bloques-react/tree/${gitInfo.commit.hash}`
+    return <span>Versión {process.env.REACT_APP_VERSION} - <Link to={repoUrl} target="_blank">{gitInfo.commit.shortHash}</Link></span>
 }
-export const Footer = () =>
-  <Stack direction={{ xs: 'column', sm: 'row' }} spacing={12} justifyContent={"center"} sx={{ padding: '10px' }}>
-    <Grid >
-      <Version />
-      <br />
-      <Links />
-    </Grid>
-    <Box sx={{ padding: "10px", [theme.breakpoints.up('sm')]: {width:"30rem"} }}>
-      <FooterLogos />
-    </Box>
-  </Stack>
+
+const Links = () =>{
+    const {t} = useTranslation("footer")
+    const termsAndConditionsLink = "https://docs.google.com/document/u/1/d/e/2PACX-1vTNX9zl8txZmuINNz2qODrodoQhvr0o2-r3T_6yFp6quEpidmPz6ORx1HSjo2KNUg6MnyHPN-Ti44z1/pub"
+    
+    return <>
+        <Link to="acercade">{t("aboutPilasBloques")}</Link> |
+        <Link to="https://pilasbloques.program.ar/docentes"> {t("toTeachersSite")}</Link> |
+        <Link to={termsAndConditionsLink}> {t("terms")}</Link>
+    </>
+}
+
+export const Footer = () => 
+    <Stack direction="row" justifyContent={"center"} alignItems={"center"} spacing={12} height={"6rem"}>
+
+        <Grid> 
+            <Typography fontSize={"18px"}>
+                <Version/>
+                <br/>
+                <Links/>
+            </Typography>
+        </Grid>
+
+        <FooterLogos width="30rem"/>
+
+    </Stack>
