56072b09fbdae47e33f021f2d72af90e4e476b72
Tomas Floxo <tomasfloxolodeiro@gmail.com>
2023-05-16 11:37:04 -0300

    tab to space bookcards

--- src/components/home/BookCards.tsx
+++ src/components/home/BookCards.tsx
@@ -3,17 +3,17 @@ import { LinkCard } from "./HomeCard"
 import { useTranslation } from "react-i18next"
 
 const InitialBookCard = () => {
-    const {t} = useTranslation("home")
+	const {t} = useTranslation("home")
 	return <LinkCard url="/libros/1" text={t("cards.initial")} color="#FCE43E" image={"toto-initial-level.svg"} />
 }
 
 const IntermediateBookCard = () => {
-    const {t} = useTranslation("home")
+	const {t} = useTranslation("home")
 	return <LinkCard url="/libros/2" text={t("cards.intermediate")} color="#53BF24" image={"toto-intermediate-level.svg"} />
 }
 
 const AdvancedBookCard = () => {
-    const {t} = useTranslation("home")
+	const {t} = useTranslation("home")
 	return <LinkCard url="/libros/100" text={t("cards.advanced")} color="#32CFC1" image={"toto-advanced-level.svg"} />
 }
 
