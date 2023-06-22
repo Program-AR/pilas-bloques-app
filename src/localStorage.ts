b4606cd90fab9e92ff8a0864c6ec7a839618c707
Diana López Alvas <dlopezalvas@gmail.com>
2023-06-21 23:46:58 -0300

    fixing storage bug

--- src/localStorage.ts
+++ src/localStorage.ts
@@ -19,22 +19,22 @@ export namespace LocalStorage {
 
     export const getSelectedLocale = (): LanguageCode | null => _get(PB_SELECTED_LOCALE)
     export const getImportedChallenge = (): EmberExecutableChallenge | null => _get(PB_IMPORTED_CHALLENGE)
-    export const getSelectedLanguageKey = (): LanguageCode | null => _get(SELECTED_LANGUAGE_KEY)
+    export const getSelectedLanguageKey = (): LanguageCode | null => _get(SELECTED_LANGUAGE_KEY, false)
     export const getUser = (): User | null => _get(PB_USER)
     export const getCreatorChallenge = (): CreatorChallenge | null => _get(PB_CREATOR_CHALLENGE)
 
     export const saveSelectedLocale = (selectedLocale: LanguageCode) => _save(PB_SELECTED_LOCALE, selectedLocale)
     export const saveImportedChallenge = (importedChallenge: EmberExecutableChallenge) => _save(PB_IMPORTED_CHALLENGE, importedChallenge)
-    export const saveSelectedLanguageKey = (key: LanguageCode) => _save(SELECTED_LANGUAGE_KEY, key)
+    export const saveSelectedLanguageKey = (key: LanguageCode) => _save(SELECTED_LANGUAGE_KEY, key, false)
     export const saveUser = (user: User | null) => _save(PB_USER, user)
     export const saveCreatorChallenge = (challenge: CreatorChallenge | null) => _save(PB_CREATOR_CHALLENGE, challenge)
 
-    const _get = (key: string) => _doSafe((storage: Storage) => {
-        const value: string | null = storage.getItem(key)
-        return value ? JSON.parse(value) : null
+    const _get = (key: string, shouldParse: boolean = true) => _doSafe((storage: Storage) => {
+        const value = storage.getItem(key)
+        return value && shouldParse ? JSON.parse(value) : value
     })
 
-    const _save = (key: string, data: any = null) => { _doSafe((storage: Storage) => storage.setItem(key, JSON.stringify(data))) }
+    const _save = (key: string, data: any = null, shouldStringify: boolean = true) => { _doSafe((storage: Storage) => storage.setItem(key, (shouldStringify ? JSON.stringify(data) : data))) }
 
     const _doSafe = <T>(fn: (storage: Storage) => T) => {
         try {
