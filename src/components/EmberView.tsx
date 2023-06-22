c71adb23ffd9719800b20b82363994d826dc32dd
Daniel Ferro <danieloscarferro@gmail.com>
2023-06-22 17:18:01 -0300

    adding Box container to style iframe

--- src/components/EmberView.tsx
+++ src/components/EmberView.tsx
@@ -1,8 +1,12 @@
+import { Box } from "@mui/material"
+
 type EmberViewProps = {
     path: string
 }
 
 export const EmberView = (props: EmberViewProps) => {
 
-    return <iframe id="ember-iframe" title='ember-view' src={`emberPB/index.html#/${props.path}`} width='100%' height='100%'/>
+    return <Box style={{marginTop:"0px", display: "flex", height: "100%"}}>
+              <iframe id="ember-iframe" title='ember-view' src={`emberPB/index.html#/${props.path}`} width='100%' height='100%'/>
+            </Box> 
 }
