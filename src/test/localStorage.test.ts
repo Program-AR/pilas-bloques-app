e2b0157fe00718e461296173622920d917dde03d
Diana López Alvas <dlopezalvas@gmail.com>
2023-06-23 09:18:46 -0300

    pr comments

--- src/test/localStorage.test.ts
+++ src/test/localStorage.test.ts
@@ -12,10 +12,17 @@ describe('Storage', () => {
         answeredQuestionIds: [1, 2]
     }
 
+    const originalConsoleError = console.log
+
     beforeEach(() => {
         localStorage.clear()
         localStorage.setItem('PB_USER', JSON.stringify(fakeUser))
+        console.error = jest.fn()
     });
+
+    afterEach(() => {
+        console.error = originalConsoleError
+    })
 
     test('Should get localStorage data', () => {
         expect(LocalStorage.getUser()).toEqual(fakeUser)
@@ -32,10 +39,12 @@ describe('Storage', () => {
         expect(LocalStorage.getUser()).toBeNull()
     })
 
-    test('On fails, should clean localStorage', () => {
+    test('On fails, should remove problematic key', () => {
+        LocalStorage.saveSelectedLocale('en-us')
         localStorage.setItem('PB_USER', "123{ASD")
         LocalStorage.getUser()
         expect(LocalStorage.getUser()).toBeNull()
+        expect(LocalStorage.getSelectedLocale()).not.toBeNull()
     })
 
 })
