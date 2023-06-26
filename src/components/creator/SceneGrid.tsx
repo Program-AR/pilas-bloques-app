352468f0b825a9705d14432c282a64a7993b68e9
Diana López Alvas <dlopezalvas@gmail.com>
2023-06-26 17:46:44 -0300

    key errors fixed

--- src/components/creator/SceneGrid.tsx
+++ src/components/creator/SceneGrid.tsx
@@ -17,16 +17,16 @@ export const SceneGrid = () => {
     const challenge: SerializedChallenge | null = LocalStorage.getCreatorChallenge()
 
     const maps: SceneMap[] = challenge ? challenge.scene.maps : defaultMaps
-    
+
     return <Stack>
-        {maps[0].map(row =>
-            <Stack direction="row">
-                {row.map(cellContent => <Cell>{cellContent}</Cell>)}
+        {maps[0].map((row, i) =>
+            <Stack  key={i} direction="row" data-testid="challenge-row">
+                {row.map((cellContent, j) => <Cell key={j} testId="challenge-cell">{cellContent}</Cell>)}
             </Stack>)}
     </Stack>
 }
 
 //May be of use in the future to try Grid with aspect-ratio: 1/1 to force the cell to be squared
-const Cell: React.FC<Props> = (props) => <div style={{ borderStyle: "solid", height: "100%", width: "100%" }}>{props.children}</div>
+const Cell: React.FC<CellProps> = (props) => <div data-testid={props.testId} style={{ borderStyle: "solid", height: "100%", width: "100%" }}>{props.children}</div>
 
-interface Props { children: React.ReactNode }
+interface CellProps { children: React.ReactNode, testId: string}
