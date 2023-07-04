e164ef5ccd5b9c2b51e90dc59dd514f44cc8383c
Diana López Alvas <dlopezalvas@gmail.com>
2023-07-03 22:31:47 -0300

    it's something

--- src/components/creator/SceneGrid.tsx
+++ src/components/creator/SceneGrid.tsx
@@ -9,54 +9,44 @@ export const SceneGrid = () => {
     const challenge: SerializedChallenge | null = LocalStorage.getCreatorChallenge()
 
     const maps: SceneMap[] = challenge ? challenge.scene.maps : defaultMaps
-    console.log(maps)
 
     const sceneType: SceneType = challenge!.scene.type
 
-    return <Stack className={styles.grid + ' ' + styles.border} >
+    return <Stack className={styles.grid} >
         {maps[0].map((row, i) =>
             <Stack key={i + row.join(',')} direction="row" data-testid="challenge-row">
                 {row.map((cellContent, j) =>
-                    <Cell key={i * 100 + j + cellContent} testId="challenge-cell" sceneType={sceneType}>
-                        <Content content={cellContent} sceneType={sceneType}></Content>
-                    </Cell>)}
+                    <SceneCell
+                        key={i * 100 + j + cellContent}
+                        content={cellContent}
+                        sceneType={sceneType} />)}
             </Stack>)}
     </Stack>
 }
 
-const imagePath = `imagenes/sceneImages`
+interface CellProps {
+    content: string,
+    sceneType: SceneType
+}
 
-interface ContentProps { content: string, sceneType: SceneType }
+const SceneCell: React.FC<CellProps> = (props) => {
 
-const Content: React.FC<ContentProps> = (props) => {
+    const imagePath = `imagenes/sceneImages/${props.sceneType}`
+    const backgroundCellImage = `${imagePath}/casilla.png`
 
-    const objectsInCell = props.content.split('&')
-    console.log(objectsInCell)
+    const objectsInCell = props.content.split('&').filter(o => o !== '-')
+    const objectStyle = (object: string) => styles[`img-${object}`] || styles['img-default']
 
-    const objectStyle = (object: string) => styles['img-default'] + ' ' + styles[`img-${object}`]
-
-    return <div className="content-images">
-        {objectsInCell.map((obj, i) =>
+    return <div
+        data-testid="challenge-cell"
+        className={styles.cell}
+        style={{ backgroundImage: `url(${backgroundCellImage})` }}>
+        {objectsInCell.map(obj =>
             <img
-                key={i + obj}//TODO revisar jey para que tenga la de la cell
-                src={`${imagePath}/${props.sceneType}/${obj}.png`}
+                key={'&'+obj}
+                src={`${imagePath}/${obj}.png`}
                 alt={obj}
                 className={objectStyle(obj)} />
         )}
     </div>
-
 }
-
-const Cell: React.FC<CellProps> = (props) => {
-
-    const image = `${imagePath}/${props.sceneType}/casilla.png`
-
-    return <div
-        data-testid={props.testId}
-        className={styles.cell}
-        style={{ backgroundImage: `url(${image})` }}>
-        {props.children}
-    </div>
-}
-
-interface CellProps { children: React.ReactNode, testId: string, sceneType: SceneType }
