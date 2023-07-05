86b1b0cbf15d04bb17766ebf502d07cb95723f36
Daniel Ferro <danieloscarferro@gmail.com>
2023-07-05 15:48:10 -0300

    opt paths

--- src/components/creator/SceneTools.tsx
+++ src/components/creator/SceneTools.tsx
@@ -19,6 +19,9 @@ export const SceneTools = () => {
        setSelectedTool(e.currentTarget.id as any)
     }
 
+    const imagePath = 'imagenes/sceneImages'
+    const imagePathScene = `${imagePath}/${challenge?.scene.type}`
+
     const Tool = (props: ToolProps) => 
         <Button variant="outlined" 
                 id={props.id}
@@ -30,10 +33,11 @@ export const SceneTools = () => {
                         boxShadow: `0 0px calc(10px * ${Number(props.id===selectedTool)})`,
                         borderRadius: "2",  width:"50px", height:"50px"}}/>
     
+
     const PutObstacleTool = () => 
         <>
            <Typography variant="caption">{t('tools.putObstacle')}</Typography>
-            <Tool id="O" image={`imagenes/sceneImages/${challenge?.scene.type}/O.png`} /> 
+            <Tool id="O" image={`${imagePathScene}/O.png`} /> 
         </>
 
     const PutObjectTool = () => 
@@ -41,20 +45,20 @@ export const SceneTools = () => {
             <Typography variant="caption">{t('tools.putObject')}</Typography>
             <Stack direction="row" style={{flexWrap: "wrap", justifyContent: "center"}}>
                 {sceneObjects(challenge!.scene.type).map((object) => 
-                <Tool id={object} image={`imagenes/sceneImages/${challenge?.scene.type}/${object}.png`}/>)}
+                <Tool id={object} image={`${imagePathScene}/${object}.png`}/>)}
             </Stack>
         </>
 
     const PutActorTool = () => 
         <>
             <Typography variant="caption">{t('tools.putActor')}</Typography>
-            <Tool id="A" image={`imagenes/sceneImages/${challenge?.scene.type}/A.png`}/>
+            <Tool id="A" image={`${imagePathScene}/A.png`}/>
         </>
                         
     const DeleteTool = () => 
         <>
             <Typography variant="caption">{t('tools.delete')}</Typography>
-            <Tool id="e" image={`imagenes/sceneImages/eraser.png`}/>
+            <Tool id="e" image={`${imagePath}/eraser.png`}/>
         </>     
 
 
