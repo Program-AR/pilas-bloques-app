217be25af66f98798087ab82742cd271c5000eb5
Tomas Floxo <tomasfloxolodeiro@gmail.com>
2023-07-12 15:50:42 -0300

    sharebutton

---
+++ src/components/creator/ShareButton.tsx
@@ -0,0 +1,27 @@
+import { Button } from "@mui/material";
+import { LocalStorage } from "../../localStorage";
+import { SerializedChallenge } from "../serializedChallenge";
+
+export const ShareButton = () => <Button onClick={downloadChallenge}>Compartir</Button>
+
+const downloadChallenge = () => {
+    const challenge: SerializedChallenge = LocalStorage.getCreatorChallenge()!
+  
+    const blob = new Blob([JSON.stringify(challenge)], { type: 'application/json' });
+  
+    const temporaryAnchorElement = document.createElement('a')
+    
+    const urlObject = URL.createObjectURL(blob)
+  
+    temporaryAnchorElement.href = urlObject
+  
+    const challengeName: string = challenge.title ? challenge.title : "pilas-bloques"
+  
+    temporaryAnchorElement.download = `${challengeName}.json`
+  
+    temporaryAnchorElement.click();
+  
+    // Clean up the resource
+    URL.revokeObjectURL(urlObject);
+  
+}
