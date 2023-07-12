31b10dec56540ede44815f15d488aac4c7cd4572
Tomas Floxo <tomasfloxolodeiro@gmail.com>
2023-07-12 15:49:03 -0300

    Moving share button to another file

--- src/components/creator/Editor.tsx
+++ src/components/creator/Editor.tsx
@@ -3,8 +3,7 @@ import { Header } from "../header/Header";
 import { SceneEdition } from "./SceneEdition";
 import { TitleEdition } from "./TitleEdition";
 import { StatementEdition } from "./StatementEdition";
-import { LocalStorage } from "../../localStorage";
-import { SerializedChallenge } from "../serializedChallenge";
+import { ShareButton } from "./ShareButton";
 
 export const CreatorEditor = () => {
   return (
@@ -28,7 +27,7 @@ const EditorHeader: React.FC = () =>
 const Actions = () => <Stack direction="row">
   <Button>Nuevo Desafío</Button>
   <Button>Probar</Button>
-  <Button onClick={downloadChallenge}>Compartir</Button>
+  <ShareButton/>
 </Stack>
 
 const ChallengeDetailsEdition = () => <Stack direction="row" style={{maxHeight: 70, justifyContent:"space-between"}}>
@@ -36,25 +35,3 @@ const ChallengeDetailsEdition = () => <Stack direction="row" style={{maxHeight: 
   <Button>Panel de instrucciones</Button>
   <Button>Bloques iniciales</Button>
 </Stack>
-
-const downloadChallenge = () => {
-  const challenge: SerializedChallenge = LocalStorage.getCreatorChallenge()!
-
-  const blob = new Blob([JSON.stringify(challenge)], { type: 'application/json' });
-
-  const temporaryAnchorElement = document.createElement('a')
-  
-  const urlObject = URL.createObjectURL(blob)
-
-  temporaryAnchorElement.href = urlObject
-
-  const challengeName: string = challenge.title ? challenge.title : "pilas-bloques"
-
-  temporaryAnchorElement.download = `${challengeName}.json`
-
-  temporaryAnchorElement.click();
-
-  // Clean up the resource
-  URL.revokeObjectURL(urlObject);
-
-}
