89e96f50bac9a09f2ba828a077a95162fa714b17
Tomas Floxo <tomasfloxolodeiro@gmail.com>
2023-07-24 21:09:04 -0300

    removing use effect

--- src/components/creator/Editor/SceneEdition/IncDecButtons.tsx
+++ src/components/creator/Editor/SceneEdition/IncDecButtons.tsx
@@ -2,8 +2,9 @@ import { useEffect, useState } from 'react';
 import { Box, Typography, Button, ButtonGroup } from '@mui/material';
 
 type IncDecButtonsProps = {
-    returnValue: (val:number) => void
-    initialValue?: number
+    add: () => void
+    remove: () => void
+    value: number
     min?: number
     max?: number
     label?: string | null
@@ -11,31 +12,26 @@ type IncDecButtonsProps = {
 }
 
 export const IncDecButtons = (props: IncDecButtonsProps) => {
-    const [value, setValue] = useState((props.initialValue === undefined) ? 0 : props.initialValue)
 
     const handleValue = (inc: boolean) => {
         if(inc)
         {
-          if (props.max === undefined || value < props.max)
-            setValue(value+1)
+          if (props.max === undefined || props.value < props.max)
+            props.add()
         }
         else {
-          if (props.min === undefined || value > props.min)
-            setValue(value-1)
+          if (props.min === undefined || props.value > props.min)
+            props.remove()
 
         }
     }
-    
-    useEffect(() => {
-        props.returnValue(value)
-    }, [value, props]); 
-    
+
     return (
         <Box sx={{ display:"flex", flexDirection: "column", alignItems: "center"}}>
             {props.label && <Typography variant="caption">{props.label}</Typography>}
             <ButtonGroup variant="contained" size="small" color="inherit" aria-label="small outlined button group">
                 <Button style={{ fontSize: 'large' }} onClick={()=>handleValue(false)}data-testid={`dec-btn-${props.testId}`}>-</Button>
-                <Box sx={{ display: "flex", alignItems: "center", justifyContent: "center", width:"45px" }} >{value}</Box>
+                <Box sx={{ display: "flex", alignItems: "center", justifyContent: "center", width:"45px" }} >{props.value}</Box>
                 <Button style={{ fontSize: 'large' }} onClick={()=>handleValue(true)} data-testid={`inc-btn-${props.testId}`}>+</Button>
             </ButtonGroup>
         </Box>
