475adb472997a1f998d688cb6821673401e9a8bb
Daniel Ferro <danieloscarferro@gmail.com>
2023-07-27 13:46:11 -0300

    colors and sizes

--- src/components/PBCard.tsx
+++ src/components/PBCard.tsx
@@ -10,6 +10,6 @@ export const PBCard = (props: PBCardProps & PaperProps) =>
     <Paper 
         {...props} 
         elevation={3} 
-        sx={{ ...props.sx, display: 'flex', alignItems: 'center', margin: theme.spacing(1)}}>
+        sx={{ ...props.sx, backgroundColor:'var(--theme-background-color)', display: 'flex', alignItems: 'center', margin: theme.spacing(1)}}>
             {props.children}
     </Paper>
