616cda1f4b3a5bb14239ddcb58780ed2e7eb79a8
Daniel Ferro <danieloscarferro@gmail.com>
2023-07-27 10:13:02 -0300

    themebackgroundcolor

--- src/components/creator/Editor/EditorFooter/CreatorFooter.tsx
+++ src/components/creator/Editor/EditorFooter/CreatorFooter.tsx
@@ -1,6 +1,6 @@
 import { Stack } from "@mui/material";
 
 export const CreatorFooter = ({children}: {children: React.ReactNode}) =>
-    <Stack component="footer" style={{backgroundColor: "white"}} direction="row" position="sticky" bottom="0" width="100%" maxHeight="70" justifyContent="space-evenly" alignItems="center" margin="5px">
+    <Stack component="footer" style={{backgroundColor: "var(--theme-background-color)"}} direction="row" position="sticky" bottom="0" width="100%" maxHeight="70" justifyContent="space-evenly" alignItems="center" margin="5px">
         {children}
     </Stack>
