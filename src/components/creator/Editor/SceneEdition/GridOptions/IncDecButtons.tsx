90d196479c25e81fc5f1ab1553bc45d28ad13e75
Daniel Ferro <danieloscarferro@gmail.com>
2023-08-01 16:20:09 -0300

    styling incDecButtons

--- src/components/creator/Editor/SceneEdition/GridOptions/IncDecButtons.tsx
+++ src/components/creator/Editor/SceneEdition/GridOptions/IncDecButtons.tsx
@@ -1,4 +1,4 @@
-import { Box, Typography, Button, ButtonGroup } from '@mui/material';
+import { Box, Typography, Button, ButtonGroup, useMediaQuery } from '@mui/material';
 
 type IncDecButtonsProps = {
     add: () => void
@@ -11,6 +11,8 @@ type IncDecButtonsProps = {
 }
 
 export const IncDecButtons = (props: IncDecButtonsProps) => {
+    const flexDir: string = useMediaQuery('(max-width:700px)') ? "column" : "row";
+
     const handleValue = (inc: boolean) => {
         if(inc && props.value < props.max){
             props.add()
@@ -21,12 +23,12 @@ export const IncDecButtons = (props: IncDecButtonsProps) => {
     }
 
     return (
-        <Box sx={{ display:"flex", flexDirection: "column", alignItems: "center"}}>
-            {props.label && <Typography variant="caption">{props.label}</Typography>}
-            <ButtonGroup variant="contained" size="small" color="inherit" aria-label="small outlined button group">
-                <Button style={{ fontSize: 'large' }} onClick={()=>handleValue(false)}data-testid={`dec-btn-${props.testId}`}>-</Button>
+        <Box sx={{ display:"flex", flexDirection: "column", alignItems: "center", padding:"10px" }}>
+            {props.label && <Typography textAlign="center" variant="caption">{props.label}</Typography>}
+            <ButtonGroup sx={{ flexDirection: flexDir }}  variant="contained" size="small" color="inherit" aria-label="small outlined button group">
+                <Button style={{ fontSize: 'large', borderRight:"0px" }} onClick={()=>handleValue(false)}data-testid={`dec-btn-${props.testId}`}>-</Button>
                 <Box sx={{ display: "flex", alignItems: "center", justifyContent: "center", width:"45px" }} >{props.value}</Box>
-                <Button style={{ fontSize: 'large' }} onClick={()=>handleValue(true)} data-testid={`inc-btn-${props.testId}`}>+</Button>
+                <Button style={{ fontSize: 'large', borderRight:"0px" }} onClick={()=>handleValue(true)} data-testid={`inc-btn-${props.testId}`}>+</Button>
             </ButtonGroup>
         </Box>
     );
