3ca6a6d37a86f48c5d452ed8fe881415aad193d8
Tomas Floxo <37090248+tfloxolodeiro@users.noreply.github.com>
2023-08-01 09:52:53 -0300

    Update src/components/creator/Editor/EditorSubHeader/CreatorSubHeader.tsx
    Co-authored-by: Daniel Ferro <91342656+danielferro69@users.noreply.github.com>

--- src/components/creator/Editor/EditorSubHeader/CreatorSubHeader.tsx
+++ src/components/creator/Editor/EditorSubHeader/CreatorSubHeader.tsx
@@ -1,6 +1,6 @@
 import { Stack } from "@mui/material";
 
 export const CreatorSubHeader = ({children}: {children: React.ReactNode}) =>
-    <Stack direction="row" justifyContent="center" alignItems="center" marginY="5px" spacing={20}>
+   <Stack height="60px" direction="row" justifyContent="center" alignItems="center" spacing={20}>
         {children}
     </Stack>
