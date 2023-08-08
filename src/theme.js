ea8a31d2dbe262cead14d146203812a296bdd2cf
Diana López Alvas <dlopezalvas@gmail.com>
2023-08-08 18:57:47 -0300

    ups

--- src/theme.js
+++ src/theme.js
@@ -10,10 +10,7 @@ const theme = createTheme({
     palette: {
         primary: {
             main: '#777777',
-          },
-        secondary: {
-            main: '#000000'
-        }
+        },
     },
     shape: {
         borderRadius: '10px'
