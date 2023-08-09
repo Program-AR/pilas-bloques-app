579485f377efdf7deec77c8b6ff45a7ba78e6c49
Diana López Alvas <dlopezalvas@gmail.com>
2023-08-09 20:57:44 -0300

    subheader responsiveness

--- src/components/creator/Editor/EditorSubHeader/CreatorSubHeader.tsx
+++ src/components/creator/Editor/EditorSubHeader/CreatorSubHeader.tsx
@@ -1,6 +1,10 @@
 import { Stack } from "@mui/material";
 
-export const CreatorSubHeader = ({children}: {children: React.ReactNode}) =>
-   <Stack height="60px" direction="row" justifyContent="center" alignItems="center" spacing={20} sx={{backgroundColor: 'var(--theme-background-secondary-color)'}}>
-        {children}
+export const CreatorSubHeader = ({ children }: { children: React.ReactNode }) => {
+
+    return <Stack direction='row' height='60px'  justifyContent='center' sx={{ backgroundColor: 'var(--theme-background-secondary-color)', zIndex:-1}}>
+        <Stack direction='row' width='100%' alignItems='center' justifyContent='space-between' maxWidth='var(--creator-max-width)'>
+            {children}
+        </Stack>
     </Stack>
+}
