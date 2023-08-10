326f2d5f2886155d06ee97c2f3d30de6b2790c39
Diana López Alvas <dlopezalvas@gmail.com>
2023-08-09 21:35:40 -0300

    fix preview scroll

--- src/components/creator/Editor/EditorSubHeader/CreatorSubHeader.tsx
+++ src/components/creator/Editor/EditorSubHeader/CreatorSubHeader.tsx
@@ -2,7 +2,7 @@ import { Stack } from "@mui/material";
 
 export const CreatorSubHeader = ({ children }: { children: React.ReactNode }) => {
 
-    return <Stack direction='row' height='60px'  justifyContent='center' sx={{ backgroundColor: 'var(--theme-background-secondary-color)', zIndex:-1}}>
+    return <Stack direction='row' height='var(--creator-subheader-height)'  justifyContent='center' sx={{ backgroundColor: 'var(--theme-background-secondary-color)', zIndex:-1}}>
         <Stack direction='row' width='100%' alignItems='center' justifyContent='space-between' maxWidth='var(--creator-max-width)'>
             {children}
         </Stack>
