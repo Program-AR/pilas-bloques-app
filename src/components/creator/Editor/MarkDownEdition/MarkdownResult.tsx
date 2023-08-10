f99ee7e2999e230b83c496254d5c407b779cc7a8
Daniel Ferro <danieloscarferro@gmail.com>
2023-08-10 11:52:07 -0300

    styling like PB & fix theme error

--- src/components/creator/Editor/MarkDownEdition/MarkdownResult.tsx
+++ src/components/creator/Editor/MarkDownEdition/MarkdownResult.tsx
@@ -1,12 +1,13 @@
 import { useContext } from "react";
 import ReactMarkdown from "react-markdown";
-import { Box, Button } from "@mui/material";
+import { Box, Button, lighten } from "@mui/material";
 import { WbIncandescent, MenuBook } from"@mui/icons-material"
 import { PBCard } from "../../../PBCard";
 import remarkGfm from 'remark-gfm';
 import remarkemoji from 'remark-emoji';
 import { useTranslation } from "react-i18next";
 import { MarkdownContext } from "./MarkdownContext";
+import theme from '../../../../theme';
 
 export const MarkdownResult = () => {
 
@@ -22,15 +23,18 @@ export const MarkdownResult = () => {
       setMarkdownShow(markdownClue);
   };
 
-
   return (
     <PBCard sx={{ flexDirection: "row" }}>
-    <Box component="fieldset" sx={{display: "flex", width:"100%", flexDirection: "row", boxSizing: "border-box", borderRadius: '4px' }}>
-        <legend>{t('statement.markdownTitle')}</legend>
-        <img alt="actor" style={{width:"80px", height:"80px"}} src={`${urlImage}`}/>
-        <Box sx={{FlexDirection: "column", width:"100px"}}>
-        <Button onClick={onclickStatement}><MenuBook/></Button>
-        <Button disabled={!clueCheck} onClick={onclickClue}><WbIncandescent/></Button>
+    <Box component="fieldset" sx={{display: "flex", margin: theme.spacing(0.3), width:"100%", flexDirection: "row", boxSizing: "border-box", borderStyle: "solid", borderWidth: "thin", borderRadius: theme.shape.borderRadius.toString()+"px" }}>
+        <legend style={{color: theme.palette.primary.main }}>{t('statement.markdownTitle')}</legend>
+        <Box sx={{display: "flex", flexDirection: "column", justifyContent: "center" }}>
+          <img alt="actor" style={{width:"64px", height:"64px"}} src={`${urlImage}`}/>
+        </Box>
+        <Box sx={{display: "flex", flexDirection: "column", justifyContent: "center" }}> 
+          <Box sx={{display: "flex", textAlign: "center", marginRight: theme.spacing(0.6), marginLeft: theme.spacing(0.6), width: "50px", flexDirection: "column", justifyContent: "flex-start", backgroundColor: lighten(theme.palette.primary.main, 0.74)}}>
+            <Button onClick={onclickStatement} sx={{"&:hover": {backgroundColor: lighten(theme.palette.primary.main, 0.67)}, color: theme.palette.primary.main, minWidth: "50px"}}><MenuBook/></Button>
+            <Button disabled={!clueCheck} onClick={onclickClue} sx={{"&:hover": {backgroundColor: lighten(theme.palette.primary.main, 0.67)}, color:"#ebca14", minWidth: "50px"}}><WbIncandescent style={{ transform: "rotate(180deg)"}}/></Button>
+          </Box>
         </Box>
        <ReactMarkdown children={markdownShow} remarkPlugins={[remarkGfm, [remarkemoji, {
        emoticon: true }]]} />
