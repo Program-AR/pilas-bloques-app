98b022369d057b1574d8a2f81badb27feca7b61f
Diana López Alvas <dlopezalvas@gmail.com>
2023-08-22 11:32:47 -0300

    eraser + pblinks

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,8 +1,14 @@
-import { Stack, Grid } from "@mui/material";
+import { Stack, Grid, styled } from "@mui/material";
 import { useTranslation } from "react-i18next";
-import { Link } from "react-router-dom";
+import { Link, LinkProps } from "react-router-dom";
 import GitInfo from 'react-git-info/macro';
 import { Code } from "@mui/icons-material";
+
+type PBLinkProps = {
+  children: React.ReactNode
+}
+
+const PBLink = (props: LinkProps & PBLinkProps) => <Link {...props} style={{color: 'var(--theme-link-color)'}}>{props.children}</Link>
 
 const Version = () => {
     if(!process.env.REACT_APP_VERSION) throw new Error("Missing Pilas Bloques version. ENV not set")
@@ -12,7 +18,7 @@ const Version = () => {
     return <Stack direction="row">
       {t("version")} {process.env.REACT_APP_VERSION}
       <Code/>
-      <Link to={repoUrl} target="_blank">{gitInfo.commit.shortHash}</Link>
+      <PBLink to={repoUrl} target="_blank">{gitInfo.commit.shortHash}</PBLink>
     </Stack>
 }
 
@@ -21,9 +27,9 @@ const Links = () =>{
     const termsAndConditionsLink = "https://docs.google.com/document/u/1/d/e/2PACX-1vTNX9zl8txZmuINNz2qODrodoQhvr0o2-r3T_6yFp6quEpidmPz6ORx1HSjo2KNUg6MnyHPN-Ti44z1/pub"
     
     return <>
-        <Link to="acercade">{t("aboutPilasBloques")}</Link> |
-        <Link to="https://pilasbloques.program.ar/docentes"> {t("toTeachersSite")}</Link> |
-        <Link to={termsAndConditionsLink}> {t("terms")}</Link>
+        <PBLink to="acercade">{t("aboutPilasBloques")}</PBLink> |
+        <PBLink to="https://pilasbloques.program.ar/docentes"> {t("toTeachersSite")}</PBLink> |
+        <PBLink to={termsAndConditionsLink}> {t("terms")}</PBLink>
     </>
 }
 export const Footer = () =>
