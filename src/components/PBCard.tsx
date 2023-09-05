0a7c7d058d62bf7b127209a8c3e7f6095f40738c
Daniel Ferro <danieloscarferro@gmail.com>
2023-09-05 13:28:15 -0300

    Dia & Alf' fixes

--- src/components/PBCard.tsx
+++ src/components/PBCard.tsx
@@ -13,7 +13,7 @@ export const PBCard = (props: PBCardProps & PaperProps) => {
     return <Paper 
         {...props} 
         elevation={3} 
-        sx={{ backgroundColor:'var(--theme-background-color)', display: 'flex', alignItems: 'center', margin: theme.spacing(isSmallScreen?0.5:1), ...props.sx }}>
+        sx={{ backgroundColor: theme.palette.background.default, display: 'flex', alignItems: 'center', margin: theme.spacing(isSmallScreen?0.5:1), ...props.sx }}>
             {props.children}
     </Paper>
 }
