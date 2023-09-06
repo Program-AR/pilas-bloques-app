cc225e2b6a75caac3323a40771428f0449c494d3
Alfredo Sanzo <alfredo.sanzo@gmail.com>
2023-09-06 17:00:07 -0300

    Replace nunito archive fonts by a dependency

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
+import '@fontsource/nunito';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
