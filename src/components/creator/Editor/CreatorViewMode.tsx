a197fe51df02bc00b41922addda7aa996dbfdde6
Diana López Alvas <dlopezalvas@gmail.com>
2023-09-15 09:47:28 -0300

    subheader

--- src/components/creator/Editor/CreatorViewMode.tsx
+++ src/components/creator/Editor/CreatorViewMode.tsx
@@ -1,4 +1,4 @@
-import { Box, Stack } from "@mui/material"
+import { Typography } from "@mui/material"
 import { Ember } from "../../../emberCommunication"
 import { LocalStorage } from "../../../localStorage"
 import { EMBER_IMPORTED_CHALLENGE_PATH } from "../../ImportedChallengeView"
@@ -6,37 +6,31 @@ import { EmberView } from "../../emberView/EmberView"
 import { Header, HeaderText } from "../../header/Header"
 import { SerializedChallenge } from "../../serializedChallenge"
 import { useTranslation } from "react-i18next"
-import { CreatorSubHeader } from "./EditorSubHeader/CreatorSubHeader"
-import { DownloadButton } from "./ActionButtons/DownloadButton"
 import { ReturnToEditionButton } from "./ActionButtons/ReturnToEditButton"
-import { DiscardChallengeButton } from "./ActionButtons/DiscardChallengeButton"
 import { BetaBadge } from "../BetaBadge"
-
-const ViewModeSubHeader = ({challenge}: {challenge: SerializedChallenge}) =>
-    <CreatorSubHeader>
-        <Box sx={{ width: "293px" }}>
-            <HeaderText text={challenge.title}/>
-        </Box>
-        <Actions />
-    </CreatorSubHeader>
-
-const Actions = () => <>
-    <ReturnToEditionButton/>
-    <Stack direction="row" alignItems={"center"}>
-        <DiscardChallengeButton/>
-        <DownloadButton/>
-    </Stack>
-    </>
+import { PBreadcrumbs } from "../../PBreadcrumbs"
+import { EditorSubHeader } from "./Editor"
 
 export const CreatorViewMode = () => {
-    const {t} = useTranslation('creator')
 
     const challengeBeingEdited: SerializedChallenge = LocalStorage.getCreatorChallenge()!
 
     Ember.importChallenge(challengeBeingEdited)
-    
+
     return <>
-        <Header CenterComponent={<BetaBadge smaller={true}><HeaderText text={t("editor.previewModeHeader")} /></BetaBadge>} SubHeader={<ViewModeSubHeader challenge={challengeBeingEdited}/>}/>
-        <EmberView height='calc(100% - var(--creator-subheader-height))' path={EMBER_IMPORTED_CHALLENGE_PATH}/>
+        <Header CenterComponent={<CreatorViewHeader challenge={challengeBeingEdited}/>} SubHeader={<EditorSubHeader viewButton={<ReturnToEditionButton/>}/>} />
+        <EmberView height='calc(100% - var(--creator-subheader-height))' path={EMBER_IMPORTED_CHALLENGE_PATH} />
     </>
+}
+
+const CreatorViewHeader = ({ challenge }: { challenge: SerializedChallenge }) => {
+    const { t } = useTranslation('creator')
+    
+    return <BetaBadge smaller={true}>
+        <PBreadcrumbs>
+            <HeaderText text={t("editor.previewModeHeader")} />
+            <Typography>{challenge.title}</Typography>
+        </PBreadcrumbs>
+    </BetaBadge>
+
 }
