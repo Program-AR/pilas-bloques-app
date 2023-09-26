d03c7c1306b568482ce9324ec6a170d083a13fc8
Daniel Ferro <danieloscarferro@gmail.com>
2023-09-26 14:59:48 -0300

    coloring

--- src/components/PBProgress.tsx
+++ src/components/PBProgress.tsx
@@ -2,6 +2,6 @@ import { Box, CircularProgress } from "@mui/material"
 
 export const PBProgress = () => {
     return <Box justifyContent="center" display="flex" alignItems="center" height="100%">
-             <CircularProgress sx={{color: "var(--home-background)"}} thickness={2} size={150}/>
+             <CircularProgress thickness={2} size={150}/>
         </Box>
 }
