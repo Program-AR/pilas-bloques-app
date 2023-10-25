f3f15f2b2382256211f4c14104785f96a2b9bdd7
Alfredo Sanzo <alfredo.sanzo@gmail.com>
2023-10-25 18:37:41 -0300

    New Book View alive

--- src/components/book/BookView.tsx
+++ src/components/book/BookView.tsx
@@ -1,4 +1,4 @@
-import { Stack, Typography } from "@mui/material";
+import { Divider, Stack, Typography } from "@mui/material";
 import { useParams } from "react-router-dom";
 import { Book, getBook } from "../../staticData/books";
 import { Header } from "../header/Header";
@@ -7,6 +7,10 @@ import { Link } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 import { PBreadcrumbs } from "../PBreadcrumbs";
 import { Challenge } from "../../staticData/challenges";
+import { Chapter } from "../../staticData/chapters";
+import { Group } from "../../staticData/groups";
+import { PBCard } from "../PBCard";
+import { useThemeContext } from "../../theme/ThemeContext";
 
 const Breadcrumb = (book: Book) => {    
     const {t} = useTranslation("books")
@@ -26,35 +30,69 @@ const Breadcrumb = (book: Book) => {
 export const BookView = () => {
     const {id} = useParams()
     const book: Book = getBook(Number(id))
+    const {t} = useTranslation("books")
+    const {theme} = useThemeContext()
 
     return <>
         <Header CenterComponent={Breadcrumb(book)}/>
-        <ChallengeList book={book} />
+        <Stack alignItems="center" style={{backgroundImage: "url(imagenes/book-background.svg)"}}>
+            <PBCard style={{maxWidth: 'var(--creator-max-width)', padding: theme.spacing(2)}}>
+                <Stack>
+                    <Typography variant="h3">{t(`${book.id}.title`)}</Typography>
+                    {book.chapters.map( chapter => <ChapterView chapter={chapter} key={chapter.id}/>)}
+                </Stack>
+            </PBCard>
+        </Stack>
 </>
 }
 
-const ChallengeList = ({book}: {book: Book}) => {
-    const {t} = useTranslation("books")
-    return <Stack>
-        <Typography variant="h1">{t(`${book.id}.title`)}</Typography>
-        {book.chapters.map( chapter => <Stack key={chapter.id}>
-            <Typography variant="h2">{t(`chapters.${chapter.id}.title`)}</Typography>
-            {chapter.groups.map( group => {
-                var groupTitle = t(`groups.${group.id}.title`)
-                return <Stack key={group.id}>
-                    {groupTitle && <Typography variant="h3">{groupTitle}</Typography>}
-                    <Stack direction="row">
-                        {group.challenges.map( challenge => 
-                            <ChallengeCard key={challenge.id} challenge={challenge} />
-                        )}
-                    </Stack>
-                </Stack>
-            })}
-        </Stack>)}
-    </Stack>
+const ChapterView = ({chapter}: {chapter: Chapter}) => {
+    const {t} = useTranslation("chapters")
+    const {theme} = useThemeContext()
+    const Groups = () => <>{chapter.groups.map( group => <GroupView group={group} key={group.id} /> )}</>
+    return <>
+        <Divider sx={{margin: theme.spacing(1)}} variant="middle"/>
+        <Typography variant="h5" sx={{ fontWeight: 'bold' }}>{t(`${chapter.id}.title`)}</Typography>
+        {   // We stack in a row unitary groups (for intermediate and advanced books)
+            chapter.groups[0].hasOnlyOneChallenge() ? 
+                <Stack direction="row" flexWrap="wrap"><Groups/></Stack> : 
+                <Groups/>
+        }
+    </> 
 }
 
+const GroupView = ({group}: {group: Group}) => {
+    const {t} = useTranslation("groups")
+    return <>
+        {   // We don't show the title for unitary groups (for intermediate and advanced books)
+            !group.hasOnlyOneChallenge() && <Typography variant="h6">{t(`${group.id}.title`)}</Typography>
+        }
+        <Stack direction="row" flexWrap="wrap">
+            {group.challenges.map( challenge => <ChallengeCard key={challenge.id} challenge={challenge} /> ) }
+        </Stack>
+    </>
+}
+                            
 const ChallengeCard = ({challenge}:{challenge: Challenge}) => {
     const {t} = useTranslation("challenges")
-    return <Typography>{t(`${challenge.id}.title`)}</Typography>
+    const {theme} = useThemeContext()
+    return <Link to={"/desafio/" + challenge.id}>
+        <Stack width="120px">
+            <PBCard><ChallengeCover challenge={challenge}/></PBCard>
+            <Typography align="center" 
+                style={{ marginLeft: theme.spacing(0.5), marginRight: theme.spacing(0.5), marginBottom: theme.spacing(2)}}>
+                    {t(`${challenge.id}.title`)}
+            </Typography>
+        </Stack>
+    </Link>
+}
+
+const ChallengeCover = ({challenge}: {challenge: Challenge}) => {
+    const {theme} = useThemeContext()
+    return <img 
+        width="100%"
+        style={{borderRadius: theme.shape.borderRadius}}
+        alt="challenge" 
+        src={`imagenes/challengeCovers/${challenge.id}.png`} 
+    />
 }
