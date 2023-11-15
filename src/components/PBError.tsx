be5661bcb948d571f08755e0c7fbefd58435dc55
Diana López Alvas <dlopezalvas@gmail.com>
2023-11-15 18:39:34 -0300

    prettier errors

--- src/components/PBError.tsx
+++ src/components/PBError.tsx
@@ -1,6 +1,11 @@
 import { useRouteError } from "react-router-dom";
+import { PageNotFound } from "./pageNotFound/PageNotFound";
+import { useTranslation } from "react-i18next";
 
 export const PBError = () => {
+    const {t} = useTranslation("pageNotFound")
+
     const error = useRouteError() as any
-    return <h1>Ocurrio un error (✖╭╮✖): {error.message}</h1>
+
+    return <PageNotFound title={t("errorOcurred")!} error={error.message}/>
 }
--- src/components/pageNotFound/PageNotFound.tsx
+++ src/components/pageNotFound/PageNotFound.tsx
@@ -4,15 +4,21 @@ import { useTranslation } from "react-i18next";
 import { Header } from "../header/Header"
 import styles from "./pageNotFound.module.css"
 
-export const PageNotFound = () =>{
+type PageNotFoundProps = {
+    title?: string,
+    error?: string,
+}
+
+export const PageNotFound = (props: PageNotFoundProps) =>{
     const {t} = useTranslation("pageNotFound")
+
     return <>
         <Header/>
         <Stack direction='column' alignItems='center'>
-            <Typography className={styles.title} variant="h4">{t("title")}</Typography>
+            <Typography className={styles.title} variant="h4">{props.title ? props.title : t("title")}</Typography>
             <img alt="capy" className={styles.image} src='imagenes/capy_error.png' />
-            <Typography className={styles.text} variant="h6">{t("text")}</Typography>
-            <Link to="">
+            <Typography className={styles.text} variant="h6">{props.error ? props.error : t("text")}</Typography>
+            <Link to="/">
                 <Button variant="contained" color="success" >{t("home")}</Button>
             </Link>
         </Stack>
