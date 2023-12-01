49593c31d3b6189ae42859debca19a9eb32447fb
Diana López Alvas <dlopezalvas@gmail.com>
2023-12-01 15:43:07 -0300

    copy to clippboard (ugly)

--- src/components/creator/Editor/ActionButtons/ShareButton.tsx
+++ src/components/creator/Editor/ActionButtons/ShareButton.tsx
@@ -4,11 +4,13 @@ import { CreatorActionButton } from "./CreatorActionButton";
 import DownloadIcon from '@mui/icons-material/Download';
 import ShareIcon from '@mui/icons-material/Share';
 import SaveIcon from '@mui/icons-material/Save';
+import ContentCopyIcon from '@mui/icons-material/ContentCopy';
 import { PilasBloquesApi } from "../../../../pbApi";
 import { ReactNode, useContext, useState } from "react";
-import { Dialog, DialogContent, DialogTitle, Stack, Tooltip } from "@mui/material";
+import { Dialog, DialogContent, DialogTitle, Snackbar, Stack, TextField } from "@mui/material";
 import { DownloadButton } from "./DownloadButton";
 import { CreatorContext } from "../CreatorContext";
+import { IconButtonTooltip } from "../SceneEdition/IconButtonTooltip";
 
 export const ShareButton = () => {
 
@@ -22,28 +24,62 @@ export const ShareButton = () => {
 }
 
 const ShareDialog = ({ open, setDialogOpen }: { open: boolean, setDialogOpen: (open: boolean) => void }) => {
-    const { shareId } = useContext(CreatorContext)
-
     return <>
         <Dialog open={open} onClose={() => { setDialogOpen(false) }}>
             <DialogTitle>Compartir desafio</DialogTitle>
             <DialogContent>
-                <Stack>
-                    { `http://localhost:3000/#/desafio/guardado/${shareId}`
-                }
-                    <Buttons/>
-                </Stack>
+                <ShareModal />
             </DialogContent>
-        </Dialog>
+        </Dialog >
     </>
 }
+
+const ShareModal = () => {
+    const { shareId } = useContext(CreatorContext)
+
+    const link: string = `http://localhost:3000/#/desafio/guardado/${shareId}`
+
+    return <Stack>
+        <Stack direction='row' justifyContent='space-around' sx={{margin: 1}}>
+            <TextField
+                defaultValue={link}
+                InputProps={{ readOnly: true }}
+            />
+            <CopyToClipboardButton textToCopy={link} />
+        </Stack>
+        <Buttons />
+    </Stack>
+
+}
+
+const CopyToClipboardButton = ({ textToCopy }: { textToCopy: string }) => {
+
+    const [open, setOpen] = useState(false)
+
+    const handleClick = () => {
+        setOpen(true)
+        navigator.clipboard.writeText(textToCopy)
+    }
+
+    return <>
+        <IconButtonTooltip sx={{}} icon={<ContentCopyIcon />} onClick={handleClick} tooltip={"Copiar al portapapeles"}/>
+        <Snackbar
+            open={open}
+            onClose={() => setOpen(false)}
+            autoHideDuration={2000}
+            message="Copied to clipboard"
+        />
+    </>
+}
+
+export default CopyToClipboardButton
 
 const Buttons = () => {
     const { shareId } = useContext(CreatorContext)
 
     return <>
-        <Stack direction="row" justifyContent="space-between">
-            {shareId ? <SaveButton /> : <ShareUrlButton/>}
+        <Stack direction="row" justifyContent="space-between" alignItems='center'>
+            {shareId ? <SaveButton /> : <ShareUrlButton />}
             <DownloadButton />
         </Stack>
     </>
@@ -54,11 +90,11 @@ const ShareUrlButton = () => {
     const shareChallenge = async (): Promise<string> => {
         const challenge: SerializedChallenge = LocalStorage.getCreatorChallenge()!
         const sharedChallenge = await PilasBloquesApi.shareChallenge(challenge)
-        
+
         return sharedChallenge.sharedId
     }
 
-    return <ChallengeUpsertButton Icon={<ShareIcon />} nametag="shareUrl" challengeUpsert={shareChallenge}/>
+    return <ChallengeUpsertButton Icon={<ShareIcon />} nametag="shareUrl" challengeUpsert={shareChallenge} />
 }
 const SaveButton = () => {
 
@@ -67,10 +103,10 @@ const SaveButton = () => {
         return savedChallenge.sharedId
     }
 
-    return <ChallengeUpsertButton Icon={<SaveIcon />} nametag="save" challengeUpsert={saveChallenge}/>
+    return <ChallengeUpsertButton Icon={<SaveIcon />} nametag="save" challengeUpsert={saveChallenge} />
 }
 
-const ChallengeUpsertButton = ({Icon, challengeUpsert, nametag}: {Icon: ReactNode, nametag: string, challengeUpsert: () => Promise<string>}) => {
+const ChallengeUpsertButton = ({ Icon, challengeUpsert, nametag }: { Icon: ReactNode, nametag: string, challengeUpsert: () => Promise<string> }) => {
 
     const { setShareId } = useContext(CreatorContext)
     const userLoggedIn = !!LocalStorage.getUser()
