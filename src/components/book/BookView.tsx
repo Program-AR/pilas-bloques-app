764210cee31c2c43954c892a6dd1fd7b40fc3f7a
Rocio Gonzalez <rgonzalezt@outlook.com>
2024-01-09 15:08:45 -0300

    New layout and names

--- src/components/book/BookView.tsx
+++ src/components/book/BookView.tsx
@@ -1,4 +1,4 @@
-import { Divider, Stack, Typography } from "@mui/material";
+import { Divider, Stack, Typography, useMediaQuery } from "@mui/material";
 import { useParams } from "react-router-dom";
 import { Book, getBook } from "../../staticData/books";
 import { Header } from "../header/Header";
@@ -62,7 +62,7 @@ const ChapterView = ({chapter}: {chapter: Chapter}) => {
         <Typography variant="h5" sx={{ fontWeight: 'bold' }}>{t(`${chapter.id}.title`)}</Typography>
         {   // We stack in a row unitary groups (for intermediate and advanced books)
             chapter.groups[0].hasOnlyOneChallenge() ? 
-                <Stack direction="row" flexWrap="wrap"><Groups/></Stack> : 
+                <ChallengeRow><Groups/></ChallengeRow> : 
                 <Groups/>
         }
     </> 
@@ -70,25 +70,33 @@ const ChapterView = ({chapter}: {chapter: Chapter}) => {
 
 const GroupView = ({group}: {group: Group}) => {
     const {t} = useTranslation("groups")
-    return <>
+    return <Stack>
         {   // We don't show the title for unitary groups (for intermediate and advanced books)
             !group.hasOnlyOneChallenge() && <Typography variant="h6">{t(`${group.id}.title`)}</Typography>
         }
-        <Stack direction="row" flexWrap="wrap">
+        <ChallengeRow>
             {group.challenges.map( challenge => <ChallengeCard key={challenge.id} challenge={challenge} /> ) }
-        </Stack>
-    </>
+        </ChallengeRow>
+    </Stack>
+}
+
+const ChallengeRow: React.FC <{children: React.ReactNode}> = ({children}) => {
+    const { theme } = useThemeContext()
+    const isVerySmallScreen: boolean = useMediaQuery(theme.breakpoints.down("sm"));
+    return <Stack direction="row" flexWrap="wrap"  justifyContent={isVerySmallScreen ? "space-evenly" : "flex-start"}>
+        {children}
+    </Stack>
 }
                             
 const ChallengeCard = ({challenge}:{challenge: Challenge}) => {
     const {t} = useTranslation("challenges")
     const {theme} = useThemeContext()
-    const space: number = 1 //8px
+    const space: number = 2 //8px
     return <Link to={"/desafio/" + challenge.id}>
-        <Stack width={100 + space * 2 * 8}>
+        <Stack alignItems="center" width={100 + space * 2 * 8}>
             <PBCard><ChallengeCover challenge={challenge}/></PBCard>
             <Typography align="center" lineHeight={1}
-                style={{ marginLeft: theme.spacing(space), marginRight: theme.spacing(space), marginBottom: theme.spacing(2)}}>
+                style={{ marginLeft: theme.spacing(space), marginRight: theme.spacing(space), marginBottom: theme.spacing(1)}}>
                     {t(`${challenge.id}.title`)}
             </Typography>
         </Stack>
