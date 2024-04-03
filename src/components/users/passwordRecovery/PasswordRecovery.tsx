01fb2d13f311f7429d19b30543e915e72a650ef6
Diana López Alvas <dlopezalvas@gmail.com>
2024-04-03 12:52:20 -0300

    change password card

--- src/components/users/passwordRecovery/PasswordRecovery.tsx
+++ src/components/users/passwordRecovery/PasswordRecovery.tsx
@@ -6,6 +6,8 @@ import { DialogSnackbar } from "../../dialogSnackbar/DialogSnackbar";
 import { PilasBloquesApi } from "../../../pbApi";
 import { useTranslation } from "react-i18next";
 import { PBMailLink, UserCard, UserTextField } from "../userForm";
+import { useThemeContext } from "../../../theme/ThemeContext";
+import { PasswordInput } from "../register/Register";
 
 export const PasswordRecovery = () => {
     const [searchParams] = useSearchParams();
@@ -13,15 +15,33 @@ export const PasswordRecovery = () => {
 
     const { t } = useTranslation('passwordRecovery');
 
-
     const [serverError, setServerError] = useState<boolean>(false)
-    const [mailSent, setMailSent] = useState<boolean>(false)
+
+    return <>
+        <Header />
+        {!token ?
+            <SendEmail setServerError={setServerError} />
+            : <NewPassword setServerError={setServerError}/>}
+        <DialogSnackbar
+            open={serverError}
+            onClose={() => setServerError(false)}
+            message={t('serverError')} />
+    </>
+}
+
+type PasswordRecoveryProps = {
+    setServerError: (serverError: boolean) => void,
+}
+
+const SendEmail = ({ setServerError }: PasswordRecoveryProps) => {
+
+    const { t } = useTranslation('passwordRecovery');
     const [userIdentifier, setUserIdentifier] = useState<string>('')
+    const [mailSent, setMailSent] = useState<boolean>(false)
 
     const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
         event.preventDefault()
         try {
-            console.log(token)
             await PilasBloquesApi.passwordRecovery(userIdentifier)
             setMailSent(true)
         } catch (error: any) {
@@ -29,26 +49,56 @@ export const PasswordRecovery = () => {
         }
     }
 
-    return <>
-        <Header />
-        <UserCard title={t("passwordRecovery")} handleSubmit={handleSubmit}>
-            {!mailSent ? <>
+    return <UserCard title={t("passwordRecovery")} handleSubmit={handleSubmit}>
+        {mailSent ? <>
+            <Typography><b>{t('mailSent')} <PBMailLink /></b></Typography>
+            <Button variant="contained" color="success" href="/#">{t("backToHome")}</Button>
+        </>
+            :
+            <>
                 <UserTextField
                     label={t("userIdentifier")}
-                    required
                     onChange={props => setUserIdentifier(props.target.value)}
+                    required
                 />
                 <Typography sx={{ width: '70%', textAlign: 'center' }}>{t("instructions")} <PBMailLink /></Typography>
                 <Button variant="contained" color="success" type='submit'>{t("passwordRecovery")}</Button>
             </>
-                : <>
-                    <Typography><b>{t('mailSent')} <PBMailLink /></b></Typography>
-                    <Button variant="contained" color="success" href="/#">{t("backToHome")}</Button>
-                </>}
-        </UserCard>
-        <DialogSnackbar
-            open={serverError}
-            onClose={() => setServerError(false)}
-            message={t('serverError')} />
-    </>
+        }
+    </UserCard>
+}
+
+const NewPassword = ({ setServerError }: PasswordRecoveryProps) => {
+    const { t } = useTranslation('passwordRecovery');
+    const { theme } = useThemeContext()
+
+    const [validPassword, setValidPassword] = useState<boolean>(false)
+    const [newPassword, setNewPassword] = useState<string>('')
+    const [confirmPasswordOk, setConfirmPasswordOk] = useState<boolean>(false)
+
+
+    const handlePassword = (password: string) => {
+        setNewPassword(password)
+    }
+
+    const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault()
+        try {
+            //await PilasBloquesApi.changePassword(userIdentifier)
+        } catch (error: any) {
+            setServerError(true)
+        }
+    }
+
+    return <UserCard title={t("newPassword")} handleSubmit={handleSubmit}>
+        <PasswordInput
+            password={newPassword}
+            handlePassword={handlePassword}
+            setValidPassword={setValidPassword}
+            confirmPasswordOk={confirmPasswordOk}
+            setConfirmPasswordOk={setConfirmPasswordOk}
+        />
+        <Button disabled={!(validPassword && confirmPasswordOk)} variant="contained" color="success" type='submit'>{t("changePassword")}</Button>
+
+    </UserCard>
 }
