6a8165758677192e594f578612c1525ae5d1a973
Diana López Alvas <dlopezalvas@gmail.com>
2024-05-09 21:42:18 -0300

    session button small screen

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,4 +1,4 @@
-import { AppBar, Grid, Typography } from "@mui/material";
+import { AppBar, Drawer, Grid, IconButton, Stack, Typography, useMediaQuery } from "@mui/material";
 import { ChangeLanguageButton } from "./ChangeLanguageButton";
 import styles from './header.module.css';
 import { SessionButton } from "../users/login/SessionButton";
@@ -6,6 +6,8 @@ import { Link } from "react-router-dom";
 import { DarkModeSwitch } from "./DarkModeSwitch";
 import { SimpleReadSwitch } from "./SimpleReadSwitch";
 import { useThemeContext } from "../../theme/ThemeContext";
+import MenuIcon from '@mui/icons-material/Menu';
+import { useState } from "react";
 
 type HeaderProps = {
     CenterComponent?: React.ReactNode,
@@ -19,28 +21,49 @@ type HeaderTextProps = {
 
 export const HeaderText = (props: HeaderTextProps) => {
 
-	return <Typography 
-		className={styles["header-text"]}>
-		{props.text}
-	</Typography>
+    return <Typography
+        className={styles["header-text"]}>
+        {props.text}
+    </Typography>
 }
 
-export const Header = ({CenterComponent= <></>, SubHeader=<></>, shouldShowSimpleReadSwitch=true}: HeaderProps) => {
+export const Header = ({ CenterComponent = <></>, SubHeader = <></>, shouldShowSimpleReadSwitch = true }: HeaderProps) => {
     const { theme } = useThemeContext()
-    
+
+    const isSmallScreen: boolean = useMediaQuery(theme.breakpoints.down('sm'));
+
     return <AppBar position="sticky" sx={{ bgcolor: theme.palette.background.default }} elevation={0}>
-            <Grid container className={styles['header']} wrap="nowrap">
-                <Link to="/" style={{display: 'flex'}}><img src="imagenes/pblogo-whiteborder.svg" className={styles['logo']} alt="logo pilas bloques"/></Link>
+        <Grid container className={styles['header']} wrap="nowrap">
+            {!isSmallScreen ? <>
+                <Link to="/" style={{ display: 'flex' }}><img src="imagenes/pblogo-whiteborder.svg" className={styles['logo']} alt="logo pilas bloques" /></Link>
                 {CenterComponent}
-                <div>
-                    <ChangeLanguageButton/>
-                    {shouldShowSimpleReadSwitch && <SimpleReadSwitch/>}
-                    <DarkModeSwitch/>
-                    <SessionButton/>
-                </div>
-            </Grid>
-            {SubHeader}
-        </AppBar>
+            </>
+                : <Menu CenterComponent={CenterComponent} />}
+            <Stack direction='row'>
+                <ChangeLanguageButton />
+                {shouldShowSimpleReadSwitch && <SimpleReadSwitch />}
+                <DarkModeSwitch />
+                <SessionButton />
+            </Stack>
+        </Grid>
+        {SubHeader}
+    </AppBar>
+}
+
+type MenuProps = {
+    CenterComponent?: React.ReactNode,
+}
+
+const Menu = ({ CenterComponent }: MenuProps) => {
+
+    const [openMenu, setOpenMenu] = useState<boolean>(false)
+
+    return <>
+        <IconButton onClick={() => setOpenMenu(true)}>
+            <MenuIcon color="primary"></MenuIcon>
+        </IconButton>
+
+    </>
 }
 
 
--- src/components/users/login/SessionButton.tsx
+++ src/components/users/login/SessionButton.tsx
@@ -1,9 +1,11 @@
-import { Button, Menu, MenuItem } from "@mui/material"
+import { Button, IconButton, Menu, MenuItem, useMediaQuery } from "@mui/material"
 import React, { useState } from "react";
 import { useTranslation } from "react-i18next";
 import styles from '../../header/header.module.css';
 import { LoginModal } from "./LoginModal";
 import { LocalStorage } from "../../../localStorage";
+import { useThemeContext } from "../../../theme/ThemeContext";
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 
 const AVATAR_COUNT = 16
 const AVATAR_PATH = "imagenes/avatars/"
@@ -16,11 +18,72 @@ export const SessionButton = () => {
 
     const [anchorElement, setAnchorElement] = React.useState<null | HTMLElement>(null);
     const [openModal, setOpenModal] = useState(false);
-    const open = Boolean(anchorElement);
+    const openLoginInMenu = Boolean(anchorElement);
+
+    const { theme } = useThemeContext()
+    const isSmallScreen: boolean = useMediaQuery(theme.breakpoints.down('sm'));
 
     const handleButtonClick = (event: React.MouseEvent<HTMLElement>) => {
-        setAnchorElement(event.currentTarget);
-        if(!isLoggedIn()) setOpenModal(true)
+        if (isLoggedIn() || isSmallScreen) {
+            setAnchorElement(event.currentTarget);
+        } else {
+            setOpenModal(true)
+        }
+    }
+
+    const isLoggedIn = () => !!getUser()
+
+    const closeModal = () => {
+        setOpenModal(false)
+    }
+
+    const closeMenu = () => {
+        setAnchorElement(null);
+    }
+
+    const getUser = () => LocalStorage.getUser()
+
+    const getRandomAvatar = () => avatars[Math.floor(Math.random() * avatars.length)]
+
+    return <>
+        <img alt='profile-avatar' className={`${styles['face']} ${!isLoggedIn() ? styles.gray : ''}`} src={!isLoggedIn() ? getRandomAvatar() : getUser()?.avatarURL} />
+        {isSmallScreen ? <>
+            <IconButton onClick={handleButtonClick}>
+                <ArrowDropDownIcon color="primary" />
+            </IconButton>
+            <Menu
+                open={openLoginInMenu}
+                anchorEl={anchorElement}
+                onClose={() => closeMenu()}>
+                <MenuItem onClick={() => { setOpenModal(true); closeMenu() }}>{t('login')}</MenuItem>
+            </Menu>
+        </>
+            :
+            <Button className={styles['login-btn']} onClick={handleButtonClick}>{isLoggedIn() ? getUser()?.nickName : t('login')}</Button>
+        }
+        <LoginModal open={openModal} onClose={closeModal} />
+        {isLoggedIn() ?
+            <LoggedInMenu
+                setOpenModal={setOpenModal}
+                anchorElement={anchorElement}
+                closeMenu={closeMenu} /> : <></>}
+    </>
+}
+
+type LoggedInMenuProps = {
+    anchorElement: null | HTMLElement,
+    setOpenModal: (openModal: boolean) => void
+    closeMenu: () => void
+}
+
+const LoggedInMenu = ({ anchorElement, setOpenModal, closeMenu }: LoggedInMenuProps) => {
+
+    const open = Boolean(anchorElement);
+    const { t } = useTranslation('login');
+
+    const handleLogOut = () => {
+        LocalStorage.saveUser(null)
+        closeMenu()
     }
 
     const handleChangeUser = () => {
@@ -28,38 +91,11 @@ export const SessionButton = () => {
         handleLogOut()
     }
 
-    const handleLogOut = () => {
-        LocalStorage.saveUser(null)
-        closeMenu()
-    }
-
-    const isLoggedIn = () => !!getUser()
-
-    const closeMenu = () => {
-        setAnchorElement(null);
-    }
-
-    const closeModal = () => {
-        setOpenModal(false)
-    }
-
-    const getUser = () => LocalStorage.getUser()
-
-    const getRandomAvatar = () => avatars[Math.floor(Math.random() * avatars.length)]
-
-
-    return <div>
-        <img alt='profile-avatar' className={`${styles['face']} ${!isLoggedIn() ? styles.gray : ''}`} src={!isLoggedIn() ? getRandomAvatar() : getUser()?.avatarURL}/>
-        <Button className={styles['login-btn']} onClick={handleButtonClick}>{isLoggedIn() ? getUser()?.nickName : t('login')}</Button>
-
-        <LoginModal open={openModal} onClose={closeModal} />
-        {isLoggedIn() ? (<Menu
-                anchorEl={anchorElement}
-                open={open}
-                onClose={closeMenu}>
-                    <MenuItem onClick={handleChangeUser}>{t('changeUser')}</MenuItem>
-                    <MenuItem onClick={handleLogOut}>{t('logout')}</MenuItem>
-                </Menu>) : <></>
-        }
-    </div>   
+    return <Menu
+        anchorEl={anchorElement}
+        open={open}
+        onClose={closeMenu}>
+        <MenuItem onClick={handleChangeUser}>{t('changeUser')}</MenuItem>
+        <MenuItem onClick={handleLogOut}>{t('logout')}</MenuItem>
+    </Menu>
 }
