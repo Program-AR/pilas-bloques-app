9bc72ca206cdbefd4e37476da7e39564ca9b9e86
Daniel Ferro <danieloscarferro@gmail.com>
2024-06-11 12:13:00 -0300

    test!

--- src/test/Creator/ToolboxPreview.test.tsx
+++ src/test/Creator/ToolboxPreview.test.tsx
@@ -4,14 +4,13 @@ import { ToolboxPreview } from '../../components/creator/Editor/ChallengeDetails
 import { commonBlocks, sceneBlocks } from '../../components/blockly/blocks';
 
 describe('Toolbox Preview', () => {
-    test.skip('rendering toolbox preview ', async () => {
+  test('rendering toolbox preview ', async () => {
+    const toolBoxItems = [...commonBlocks.map((block => block.id)), ...sceneBlocks.map((block => block.id))]
+    renderComponent(<ToolboxPreview blockIds={toolBoxItems} categorized={false} />);
+    const idPBBlockly = await screen.findByTestId('pb-blockly')
+    const injectBlockly = await screen.findByText('child-blockly')
+    expect(idPBBlockly).toBeInTheDocument()
+    expect(injectBlockly).toBeInTheDocument()
+  });
 
-      const toolBoxItems = [...commonBlocks.map((block => block.id)), ...sceneBlocks.map((block => block.id))]
-      
-      renderComponent(<ToolboxPreview blockIds={toolBoxItems} categorized={false}/>)
-      
-      const blocks = await screen.findByTestId(toolBoxItems.join(","))      
-            
-      expect(blocks).toBeInTheDocument()
-    })
 })
