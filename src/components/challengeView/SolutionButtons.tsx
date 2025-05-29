1a5ff7296bac4dccebf0248d4bd9d9990a9fe489
Diana Lopez Alvas <dlopezalvas@gmail.com>
2025-05-29 14:29:56 -0300

    adding tooltip

--- src/components/challengeView/SolutionButtons.tsx
+++ src/components/challengeView/SolutionButtons.tsx
@@ -1,33 +1,40 @@
-import { IconButton, IconButtonProps, Stack } from "@mui/material"
+import { IconButton, IconButtonProps, Stack, Tooltip } from "@mui/material"
 import DownloadIcon from '@mui/icons-material/Download';
 import FileUploadIcon from '@mui/icons-material/FileUpload';
 import ClearIcon from '@mui/icons-material/Clear';
 import { useThemeContext } from "../../theme/ThemeContext";
+import { useTranslation } from "react-i18next";
 
 export const SolutionButtons = () => {
+    const { t } = useTranslation('challenge')
+
     return <Stack sx={{ position: "absolute", zIndex: 10, right: 15, top: 15 }} direction="row" spacing={1}>
-        <SolutionButton icon={<FileUploadIcon />}/>
-        <SolutionButton icon={<DownloadIcon />}/>
-        <SolutionButton icon={<ClearIcon />}/>
+        <SolutionButton icon={<FileUploadIcon />} tooltip={t("solutionButtons.upload")} />
+        <SolutionButton icon={<DownloadIcon />} tooltip={t("solutionButtons.download")} />
+        <SolutionButton icon={<ClearIcon />} tooltip={t("solutionButtons.clear")} />
     </Stack >
 }
 
 type SolucionButtonProps = {
-    icon: React.ReactNode
+    icon: React.ReactNode,
+    tooltip: string
 }
 
 const SolutionButton = (props: SolucionButtonProps & IconButtonProps) => {
     const { theme } = useThemeContext()
 
-    return <IconButton
-        {...props}
-        sx={{
-            backgroundColor: theme.palette.background.default,
-            color: theme.palette.text.primary,
-            boxShadow: '0 2px 6px rgba(0, 0, 0, 0.2)',
-            borderRadius: '50%',
-        }}
-    >
-        {props.icon}
-    </IconButton>
+    return <Tooltip title={props.tooltip}>
+        <IconButton
+            {...props}
+            sx={{
+                backgroundColor: theme.palette.background.default,
+                color: theme.palette.text.primary,
+                boxShadow: '0 2px 6px rgba(0, 0, 0, 0.2)',
+                borderRadius: '50%',
+            }}
+        >
+            {props.icon}
+        </IconButton>
+    </Tooltip>
+
 }
