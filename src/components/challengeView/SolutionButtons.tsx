ff048e75a03aa2a8a7777f1dcd13f55a25cd188e
Diana Lopez Alvas <dlopezalvas@gmail.com>
2025-05-29 17:13:20 -0300

    download button onClick

--- src/components/challengeView/SolutionButtons.tsx
+++ src/components/challengeView/SolutionButtons.tsx
@@ -1,16 +1,18 @@
 import { IconButton, IconButtonProps, Stack, Tooltip } from "@mui/material"
 import DownloadIcon from '@mui/icons-material/Download';
-import FileUploadIcon from '@mui/icons-material/FileUpload';
 import ClearIcon from '@mui/icons-material/Clear';
 import { useThemeContext } from "../../theme/ThemeContext";
 import { useTranslation } from "react-i18next";
+import Blockly, { Block } from "blockly/core"
+import { LocalStorage } from "../../localStorage";
+import DriveFolderUploadIcon from '@mui/icons-material/DriveFolderUpload';
 
 export const SolutionButtons = () => {
     const { t } = useTranslation('challenge')
 
-    return <Stack sx={{ position: "absolute", zIndex: 10, right: 15, top: 15 }} direction="row" spacing={1}>
-        <SolutionButton icon={<FileUploadIcon />} tooltip={t("solutionButtons.upload")} />
-        <SolutionButton icon={<DownloadIcon />} tooltip={t("solutionButtons.download")} />
+    return <Stack sx={{ position: "absolute", zIndex: 10, right: 15, top: 15 }} direction="row" spacing={2}>
+        <SaveSolutionButton />
+        <SolutionButton icon={<DriveFolderUploadIcon />} tooltip={t("solutionButtons.upload")} />
         <SolutionButton icon={<ClearIcon />} tooltip={t("solutionButtons.clear")} />
     </Stack >
 }
@@ -37,4 +39,46 @@ const SolutionButton = (props: SolucionButtonProps & IconButtonProps) => {
         </IconButton>
     </Tooltip>
 
+}
+
+const SPBQ_FILE_VERSION = 1
+
+const SaveSolutionButton = () => {
+    const { t } = useTranslation('challenge')
+
+    const sanatizedTitle = () => LocalStorage.getCreatorChallenge()?.title
+        .normalize('NFD')
+        .replace(/[\u0300-\u036f]/g, '')
+        .replace(/[^a-zA-Z0-9 ]/g, '')
+        .trim()
+        .split(/\s+/)
+        .map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
+        .join('');
+
+    const activityName = sanatizedTitle() || "SinTitulo"
+
+    const fileName = `${activityName}.spbq`;
+
+    const downloadFile = (text: string, name: string, type: string) => {
+        const file = new Blob([text], { type: type });
+        const a = document.createElement('a');
+        a.href = URL.createObjectURL(file);
+        a.download = name;
+        a.type = type;
+        a.click();
+    }
+
+    const handleClick = () => {
+        const xml = Blockly.utils.xml.domToText(Blockly.Xml.workspaceToDom(Blockly.getMainWorkspace()))
+
+        const content = {
+            version: SPBQ_FILE_VERSION,
+            actividad: activityName,
+            solucion: btoa(xml)
+        };
+
+        downloadFile(JSON.stringify(content), fileName, 'application/octet-stream');
+    }
+
+    return <SolutionButton onClick={handleClick} icon={<DownloadIcon />} tooltip={t("solutionButtons.download")} />
 }
